c4728785996b88d18b1036f5977040a26708bb24
jgarciapueyo <jgarciapueyo@gmail.com>
2020-11-05 18:39:20 +0100

    Secondary Goals: Secure the endpoint WS-Security

--- src/main/java/translator/WebServiceConfig.java
+++ src/main/java/translator/WebServiceConfig.java
@@ -8,27 +8,70 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.HttpServletBean;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+import org.springframework.ws.soap.security.wss4j2.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
-import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
-import org.springframework.xml.xsd.SimpleXsdSchema;
-import org.springframework.xml.xsd.XsdSchema;
+
+import java.util.List;
+import java.util.Properties;
 
 @EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
-  @Bean
-  public ServletRegistrationBean<HttpServletBean> messageDispatcherServlet(ApplicationContext applicationContext) {
-    MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-    servlet.setApplicationContext(applicationContext);
-    servlet.setTransformWsdlLocations(true);
-    return new ServletRegistrationBean<>(servlet, "/ws/*");
-  }
+    @Bean
+    public ServletRegistrationBean<HttpServletBean> messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
 
-  @Bean(name = "translator")
-  public SimpleWsdl11Definition defaultWsdl11Definition() {
-    SimpleWsdl11Definition s = new SimpleWsdl11Definition();
-    s.setWsdl(new ClassPathResource("schemas/xjc/translator.wsdl"));
-    return s;
-  }
+    @Bean(name = "translator")
+    public SimpleWsdl11Definition defaultWsdl11Definition() {
+        SimpleWsdl11Definition s = new SimpleWsdl11Definition();
+        s.setWsdl(new ClassPathResource("schemas/xjc/translator.wsdl"));
+        return s;
+    }
+
+    // Following methods add configuration for securing the WebService with wss4j
+    // References:
+    //  - https://docs.spring.io/spring-ws/docs/3.0.11.BUILD-SNAPSHOT/reference/#security-wss4j-security-interceptor
+    //  - https://memorynotfound.com/spring-ws-username-password-authentication-wss4j/
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
+    }
+
+    /**
+     * Adds the Wss4jSecurityInterceptor (interceptor based on Apache's WSS4J).
+     * The interceptor is configured with validation actions (server) and securement actions (client).
+     *
+     * @return
+     */
+    @Bean
+    public Wss4jSecurityInterceptor securityInterceptor() {
+        Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+        securityInterceptor.setValidationActions("Timestamp UsernameToken");
+        // Validating Timestamps
+        securityInterceptor.setTimestampStrict(true);
+        securityInterceptor.setValidationTimeToLive(10);
+        // Validating Users
+        securityInterceptor.setValidationCallbackHandler(securityCallbackHandler());
+        return securityInterceptor;
+    }
+
+    /**
+     * Security Callback Handler that adds the user "root" with password "toor"
+     */
+    @Bean
+    public SimplePasswordValidationCallbackHandler securityCallbackHandler() {
+        SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
+        Properties users = new Properties();
+        users.setProperty("root", "toor");
+        callbackHandler.setUsers(users);
+        return callbackHandler;
+    }
 }
--- src/test/java/translator/web/ws/TranslatorEndpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEndpointTest.java
@@ -13,6 +13,9 @@ import org.springframework.ws.client.WebServiceIOException;
 import org.springframework.ws.client.WebServiceTransportException;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
+import org.springframework.ws.soap.client.SoapFaultClientException;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
 import translator.Application;
 import translator.web.ws.schema.GetTranslationRequest;
 import translator.web.ws.schema.GetTranslationResponse;
@@ -27,62 +30,116 @@ import static org.junit.Assert.assertNotNull;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Application.class)
 public class TranslatorEndpointTest {
 
-  private final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+    private final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
 
-  @LocalServerPort
-  private int port;
+    @LocalServerPort
+    private int port;
 
-  @Before
-  public void init() throws Exception {
-    marshaller.setPackagesToScan(ClassUtils.getPackageName(GetTranslationRequest.class));
-    marshaller.afterPropertiesSet();
-  }
+    @Before
+    public void init() throws Exception {
+        marshaller.setPackagesToScan(ClassUtils.getPackageName(GetTranslationRequest.class));
+        marshaller.afterPropertiesSet();
+    }
 
-  @Test(expected = RuntimeException.class)
-  public void testSendAndReceive() {
-    GetTranslationRequest request = new GetTranslationRequest();
-    request.setLangFrom("en");
-    request.setLangTo("es");
-    request.setText("This is a test of translation service");
-    Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
-            + port + "/ws", request);
-    assertNotNull(response);
-    assertThat(response, instanceOf(GetTranslationResponse.class));
-    GetTranslationResponse translation = (GetTranslationResponse) response;
-    assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+    // This test is useless after securing the endpoint.
+    // It has been left because it part of the Primary Goal of the assignment
+    @Test(expected = RuntimeException.class)
+    public void testSendAndReceive() {
+        GetTranslationRequest request = new GetTranslationRequest();
+        request.setLangFrom("en");
+        request.setLangTo("es");
+        request.setText("This is a test of translation service");
+        Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+                + port + "/ws", request);
+        assertNotNull(response);
+        assertThat(response, instanceOf(GetTranslationResponse.class));
+        GetTranslationResponse translation = (GetTranslationResponse) response;
+        assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
 
-    // The same information about testing in TranslatorServiceTest can be applied in this test
-  }
+        // The same information about testing in TranslatorServiceTest can be applied in this test
+    }
 
-  /**
-   * Simulate that the remote web service is not found by making a request to an URI that does not exist.
-   *
-   * This test is very specific and tests that the host exists but in that URI there is no web service listening.
-   */
-  @Test(expected = WebServiceTransportException.class)
-  public void webServiceIsNotListeningInThatURI() {
-    GetTranslationRequest request = new GetTranslationRequest();
-    request.setLangFrom("en");
-    request.setLangTo("es");
-    request.setText("This is a test of translation service");
-    Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
-            + port + "/ws-is-down", request);
+    /**
+     * Tests that the remote web service has been secured with Spring WS Security but the client does not secure the
+     * message.
+     */
+    @Test
+    public void testSendAndReceiveNoSecurityHeader() {
+        try {
+            GetTranslationRequest request = new GetTranslationRequest();
+            request.setLangFrom("en");
+            request.setLangTo("es");
+            request.setText("This is a test of translation service");
+            Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+                    + port + "/ws", request);
+        } catch (SoapFaultClientException e) {
+            assertThat(e.getFaultStringOrReason(), is("No WS-Security header found"));
+        }
+    }
 
-  }
+    /**
+     * Adds user information in the security header of the SOAP message
+     */
+    public Wss4jSecurityInterceptor securityInterceptor() {
+        Wss4jSecurityInterceptor wss4jSecurityInterceptor = new Wss4jSecurityInterceptor();
+        wss4jSecurityInterceptor.setSecurementActions("Timestamp UsernameToken");
+        wss4jSecurityInterceptor.setSecurementUsername("root");
+        wss4jSecurityInterceptor.setSecurementPassword("toor");
+        return wss4jSecurityInterceptor;
+    }
 
-  /**
-   * Simulate that the remote web service is not found by making a request to an URI that does not exist.
-   *
-   * This test is more general and includes that the host does not exist.
-   */
-  @Test(expected = WebServiceIOException.class)
-  public void webServiceIsNotInThatHost() {
-    GetTranslationRequest request = new GetTranslationRequest();
-    request.setLangFrom("en");
-    request.setLangTo("es");
-    request.setText("This is a test of translation service");
-    Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost-does-not-exist:"
-            + port + "/ws-is-down", request);
+    /**
+     * Tests that the remote web service has been secured with Spring WS Security and the client secures the
+     * message.
+     */
+    @Test
+    public void testSendAndReceiveSecurityHeader() {
+        try {
+            GetTranslationRequest request = new GetTranslationRequest();
+            request.setLangFrom("en");
+            request.setLangTo("es");
+            request.setText("This is a test of translation service");
 
-  }
+            // Add interceptors to modify the message sent from WebServiceTemplate
+            ClientInterceptor[] interceptors = new ClientInterceptor[]{securityInterceptor()};
+            WebServiceTemplate webServiceTemplate = new WebServiceTemplate(marshaller);
+            webServiceTemplate.setInterceptors(interceptors);
+
+            Object response = webServiceTemplate.marshalSendAndReceive("http://localhost:" + port + "/ws", request);
+        } catch (RuntimeException e) {
+            assertThat(e.getMessage(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+        }
+    }
+
+    /**
+     * Simulate that the remote web service is not found by making a request to an URI that does not exist.
+     * <p>
+     * This test is very specific and tests that the host exists but in that URI there is no web service listening.
+     */
+    @Test(expected = WebServiceTransportException.class)
+    public void webServiceIsNotListeningInThatURI() {
+        GetTranslationRequest request = new GetTranslationRequest();
+        request.setLangFrom("en");
+        request.setLangTo("es");
+        request.setText("This is a test of translation service");
+        Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+                + port + "/ws-is-down", request);
+    }
+
+    /**
+     * Simulate that the remote web service is not found by making a request to an URI that does not exist.
+     * <p>
+     * This test is more general and includes that the host does not exist.
+     */
+    @Test(expected = WebServiceIOException.class)
+    public void webServiceIsNotInThatHost() {
+        GetTranslationRequest request = new GetTranslationRequest();
+        request.setLangFrom("en");
+        request.setLangTo("es");
+        request.setText("This is a test of translation service");
+        Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost-does-not-exist:"
+                + port + "/ws-is-down", request);
+    }
+
+
 }
